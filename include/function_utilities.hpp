1cd29796df471396fecb4f2ef5e5ca9c88179a56
NcEternal <dizzo0111@gmail.com>
2024-03-22 16:43:42 +0000

    fused function_utilities.hpp with its source file

--- include/function_utilities.hpp
+++ include/function_utilities.hpp
@@ -11,9 +11,20 @@ namespace utils {
 	using func = std::function<double(const math::vector&)>;
 	using grad = std::function<math::vector(const math::vector&)>;
 
-	/*Test function and its gradient*/
-	double test_function(const math::vector& x);
-	math::vector test_gradient(const math::vector& x);
+	/*Test function*/
+	inline double test_function(const math::vector& x) {
+		return x[0] * x[1] + 4 * x[0] * x[0] + x[1] * x[1] + 3 * x[0];
+	}
+
+	/*Test gradient*/
+	inline math::vector test_gradient(const math::vector& x) {
+		math::vector res(x.size(), 0.);
+
+		res[0] = x[1] + 8 * x[0] + 3;
+		res[1] = x[0] + 2 * x[1];
+
+		return res;
+	}
 
 	/*Numerical gradient*/
 	inline auto auto_diff = [](const func& f, const double& h) {
